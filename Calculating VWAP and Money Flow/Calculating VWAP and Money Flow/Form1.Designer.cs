e636aa465d4302f31bf4c4f034a94e423cdd4a28
Yamil1293 <yamil.burgos1293@gmail.com>
2015-07-13 22:53:12 -0400

    All the visual studio files for this project

---
+++ Calculating VWAP and Money Flow/Calculating VWAP and Money Flow/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace CIS_4620_Financial_IT_Assignment_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblSymbol = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblVWAP = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tbSymbol = new System.Windows.Forms.TextBox();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.tbVolume = new System.Windows.Forms.TextBox();
+            this.tbVWAP = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSymbol
+            // 
+            this.lblSymbol.AutoSize = true;
+            this.lblSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSymbol.Location = new System.Drawing.Point(14, 9);
+            this.lblSymbol.Name = "lblSymbol";
+            this.lblSymbol.Size = new System.Drawing.Size(61, 20);
+            this.lblSymbol.TabIndex = 0;
+            this.lblSymbol.Text = "Symbol";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(131, 9);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(44, 20);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Price";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolume.Location = new System.Drawing.Point(246, 9);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(63, 20);
+            this.lblVolume.TabIndex = 2;
+            this.lblVolume.Text = "Volume";
+            // 
+            // lblVWAP
+            // 
+            this.lblVWAP.AutoSize = true;
+            this.lblVWAP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVWAP.Location = new System.Drawing.Point(359, 9);
+            this.lblVWAP.Name = "lblVWAP";
+            this.lblVWAP.Size = new System.Drawing.Size(56, 20);
+            this.lblVWAP.TabIndex = 3;
+            this.lblVWAP.Text = "VWAP";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(18, 58);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(135, 58);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(18, 87);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(461, 162);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // tbSymbol
+            // 
+            this.tbSymbol.Location = new System.Drawing.Point(18, 32);
+            this.tbSymbol.Name = "tbSymbol";
+            this.tbSymbol.Size = new System.Drawing.Size(100, 20);
+            this.tbSymbol.TabIndex = 7;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(135, 31);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(100, 20);
+            this.tbPrice.TabIndex = 8;
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.Location = new System.Drawing.Point(250, 31);
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(100, 20);
+            this.tbVolume.TabIndex = 9;
+            // 
+            // tbVWAP
+            // 
+            this.tbVWAP.Location = new System.Drawing.Point(363, 31);
+            this.tbVWAP.Name = "tbVWAP";
+            this.tbVWAP.Size = new System.Drawing.Size(100, 20);
+            this.tbVWAP.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 261);
+            this.Controls.Add(this.tbVWAP);
+            this.Controls.Add(this.tbVolume);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.tbSymbol);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblVWAP);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblSymbol);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSymbol;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblVWAP;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox tbSymbol;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.TextBox tbVolume;
+        private System.Windows.Forms.TextBox tbVWAP;
+    }
+}
+
---
+++ Calculating VWAP and Money Flow/Calculating VWAP and Money Flow/Form1.cs
@@ -0,0 +1,135 @@
+﻿// Form1.cs
+// Example Bloomberg API C# Subscriptions Program in Windows Forms
+// (C) 2014 Richard Holowczak
+// Portions (c) Bloomberg, LLC
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// Bloomberg API Namespaces
+using Element = Bloomberglp.Blpapi.Element;
+using Event = Bloomberglp.Blpapi.Event;
+using EventHandler = Bloomberglp.Blpapi.EventHandler;
+using EventQueue = Bloomberglp.Blpapi.EventQueue;
+using Message = Bloomberglp.Blpapi.Message;
+using Name = Bloomberglp.Blpapi.Name;
+using Service = Bloomberglp.Blpapi.Service;
+using Session = Bloomberglp.Blpapi.Session;
+using SessionOptions = Bloomberglp.Blpapi.SessionOptions;
+using Subscription = Bloomberglp.Blpapi.Subscription;
+using Request = Bloomberglp.Blpapi.Request; 
+
+namespace CIS_4620_Financial_IT_Assignment_3
+{
+    public partial class Form1 : Form
+    {
+        // Define a SessionOptions, Session and subscriptions object
+        // These are used throughout the form
+        Session session;
+        SessionOptions sessionOptions;
+        List<Subscription> subscriptions;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for the session
+        public void processBBEvent(Event eventObject, Session session)
+        {
+            // We are only interested in market data events related to
+            // our subscription
+            if (eventObject.Type == Event.EventType.SUBSCRIPTION_DATA)
+            {
+                // Each event may have multiple messages. So loop
+                // through all of the messages
+                foreach (Message msg in eventObject)
+                {
+                    // For Debugging use the display all of the fields: 
+                    System.Console.WriteLine(msg); 
+                    // If we have both LAST_TRADE and SIZE_LAST_TRADE elements then
+                    // assign the values to the text boxes
+                    if (msg.HasElement("LAST_TRADE") &&
+                        msg.HasElement("SIZE_LAST_TRADE"))
+                    {
+                        // Obtain the topic of the message
+                        string topic = (string)msg.CorrelationID.Object;
+                        try
+                        {
+                            tbPrice.Invoke(new MethodInvoker(delegate
+                            {
+                                tbPrice.Text = (string)msg.GetElementAsString("LAST_TRADE");
+                            }));
+                            tbVolume.Invoke(new MethodInvoker(delegate
+                            {
+                                tbVolume.Text = (string)msg.GetElementAsString("SIZE_LAST_TRADE");
+                            }));
+                        }
+                        catch (Exception e)
+                        {
+                            // Send an error message to console 
+                            System.Console.WriteLine("Error: " + e.ToString());
+                        }
+                    } // end if msg has both last trade and size
+                } // end loop over Messages in the eventObject
+            } // end if event type is subscription data
+        } // end processBBEvent
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bool result;
+            // Define two string constants for the security and the fields
+            // to be read. 
+            String security = "IBM US Equity";
+            String fields = "LAST_PRICE";
+            // Instantiate the SessionOptions object to hold the session parameters
+            // Note that this was defined at the Form level scope.
+            sessionOptions = new SessionOptions();
+            // Since this program will run on the same PC as the Bloomberg software,
+            //  we use “localhost” as the host name and port 8194 as the default port.
+            sessionOptions.ServerHost = "localhost";
+            sessionOptions.ServerPort = 8194;
+            // Instantiate the Session object using the sessionOptions and
+            // a reference to the event handler named processBBEvent
+            session = new Session(sessionOptions,
+                      new Bloomberglp.Blpapi.EventHandler(processBBEvent));
+            // Start the Session
+            result = session.Start();
+            // Open up the Market data Service
+            result = session.OpenService("//blp/mktdata");
+            // Instantiate the new list of subscriptions
+            subscriptions = new List<Subscription>();
+            // Get the symbol from tbSymbol text box
+            security = tbSymbol.Text;
+            // Add a subscription. Create a new CorrelationID on the fly
+            subscriptions.Add(new Subscription(security, fields, "",
+                              new Bloomberglp.Blpapi.CorrelationID(security)));
+            // Kick off the subscriptions
+            session.Subscribe(subscriptions);
+  
+        } // end btnStart_Click
+            
+            //Declaring variables for calculations
+            double dblSymbol;
+            double dblPrice;
+            double dblVolume;
+            double dblVWAP;
+
+       private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Turn off the subscription
+            session.Unsubscribe(subscriptions);
+            // Close the session
+            session.Stop();
+            // Clear text boxes
+            tbPrice.Text = String.Empty;
+            tbVolume.Text = String.Empty;
+        } // end btStart_Click
+    } // end public partial class Form1 
+} // end namespace Bloomberg_API_csharp_WinForms_RealTime
